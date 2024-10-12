2cd12c1d60ee9775f0da6aab02175f40cc79db43
abhinav <anirudhanls15@gmail.com>
2024-10-12 17:30:14 +0530

    add project2 in reac project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import React,{useState,useEffect} from 'react';
+import Gallery from './components/Gallery';
+
+function App() {
+  return (
+    <div>
+      <Gallery/>
+    </div>
+  );
+}
+
+export default App;
